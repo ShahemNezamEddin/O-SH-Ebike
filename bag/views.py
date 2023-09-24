eb5d8d15936e9748fb653b73cfbe18e72fc19cb4
ShahemNezamEddin <>
2023-09-24 12:51:22 +0000

    Added add to bag functionality #9

--- bag/views.py
+++ bag/views.py
@@ -11,13 +11,15 @@ def view_bag(request):
 
     return render(request, 'bag/bag.html')
 
+
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
-    product = get_object_or_404(Product, pk=item_id)
+   
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
+    
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
         messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
